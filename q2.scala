6f8ad6ef46cf18a4cd5ac5e9ffeb85be315bafaf
nive19s <sothilingamnivethan19@gmail.com>
2024-06-25 02:36:46 +0530

    created

---
+++ q2.scala
@@ -0,0 +1,23 @@
+object Question02{
+    def main(args : Array[String]) : Unit ={
+     var a=2
+     var b=3
+     var c=4
+     var d=5
+     var k=4.3f
+        
+     b=b-1
+     println("a)--b*a+c*d-- = " +(b*a+c*d))
+     d=d-1
+     println("b)a++ = " +a)
+     a=a+1
+     var g=0
+     println("c)-2*(g-k)+c = " +((-2)*(g-k)+c)) 
+     println("d)c=c++ = " +(c))
+     c=c+1
+     c=c+1
+     println("e)c=++c*a++ = " +(c*a))
+     a=a+1
+      
+    }
+}
