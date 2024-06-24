a1c7a44d141698c539dcc1367284b2f6a1442613
nive19s <sothilingamnivethan19@gmail.com>
2024-06-25 02:36:22 +0530

    created

---
+++ q1.scala
@@ -0,0 +1,19 @@
+object Question01{
+    def main(args: Array[String]): Unit ={
+        var i,j,k = 2
+        var m,n=5
+        var f=12.0f
+        var g=4.0f
+        var c='X'
+
+        println("a)k+12*m = " + (k+12*m));
+        println("b)m/j = " + (m.toFloat/j));
+        println("c)n%j = " + n%j);
+        println("d)m/j*j = " + m.toFloat/j*j);
+        println("e)f+10*5+g = " + (f+10*5+g));
+        i=i+1
+        println("f)++i*n = " + (i*n));
+
+
+    }
+}
